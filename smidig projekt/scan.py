46bc1e573f8173c51d1d5613c2703c45c0e252e0
Skaland <evenjoruds@gmail.com>
2024-05-28 12:02:24 +0200

    new changes

--- smidig projekt/scan.py
+++ smidig projekt/scan.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-class CurtainDropdown:
+class StyledDropdown:
     def __init__(self, root, options):
         self.root = root
         self.options = options
@@ -10,35 +10,22 @@ class CurtainDropdown:
         self.frame = ttk.Frame(root, padding="10")
         self.frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-        self.dropdown_button = ttk.Button(self.frame, text="Select Option", command=self.toggle_dropdown)
-        self.dropdown_button.grid(row=0, column=0, sticky=tk.W)
-        self.dropdown_open = False
+        self.label = ttk.Label(self.frame, text="Scan:", font=("Helvetica", 12))
+        self.label.grid(row=0, column=0, padx=(0, 10), sticky=tk.W)
 
         self.combobox = ttk.Combobox(self.frame, textvariable=self.selected_option, values=self.options)
-        self.combobox.bind("<<ComboboxSelected>>", self.on_select)
+        self.combobox.grid(row=0, column=1, sticky=(tk.W, tk.E))
         
         self.show_selected_button = ttk.Button(self.frame, text="Show Selected", command=self.show_selected)
-        self.show_selected_button.grid(row=2, column=0, sticky=tk.W)
-
-    def toggle_dropdown(self):
-        if self.dropdown_open:
-            self.combobox.grid_forget()
-        else:
-            self.combobox.grid(row=1, column=0, sticky=tk.W)
-        self.dropdown_open = not self.dropdown_open
-
-    def on_select(self, event):
-        self.dropdown_open = False
-        self.combobox.grid_forget()
-        self.dropdown_button.config(text=self.selected_option.get())
+        self.show_selected_button.grid(row=1, column=0, columnspan=2, pady=(10, 0))
 
     def show_selected(self):
         print("Selected item:", self.selected_option.get())
 
 root = tk.Tk()
-root.title("Curtain Dropdown Menu")
+root.title("Styled Dropdown Menu")
 
-options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
-dropdown = CurtainDropdown(root, options)
+options = ["windows.pslist", "windows.psscan", "windows.pstree", "windows.cmdline"]
+dropdown = StyledDropdown(root, options)
 
 root.mainloop()
