52020a2d0dc05777dde796dfdfd979624158b057
Mariataw95 <127189465+Mariataw95@users.noreply.github.com>
2024-05-28 15:11:31 +0200

    new changes done

--- smidig projekt/imageFile.py
+++ smidig projekt/imageFile.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 # Opprette hovedvinduet
 root = tk.Tk()
+root.title("Artifacts")
 
 # Opprette en etikett
 label = tk.Label(root, text="File:")  # Tittelen som sier "File"
@@ -13,4 +14,4 @@ file_entry.insert(0, "IE10WN7-2022114-202804.raw")  # Setter inn en eksempelteks
 file_entry.pack(side=tk.LEFT, padx=10, pady=10)  # Plasserer tekstfeltet i vinduet med noe padding
 
 # Starte GUI-loop
-root.mainloop()
+root.mainloop() 
