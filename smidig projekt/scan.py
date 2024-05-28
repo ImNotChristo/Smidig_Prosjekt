a386efe9b774276d36db09c043d5a543ac67aced
Skaland <evenjoruds@gmail.com>
2024-05-28 13:12:04 +0200

    added searching for options

--- smidig projekt/scan.py
+++ smidig projekt/scan.py
@@ -1,25 +1,63 @@
 import tkinter as tk
 from tkinter import ttk
 
+class SearchableCombobox(ttk.Combobox):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self._completion_list = []
+        self._hits = []
+        self._hit_index = 0
+        self.position = 0
+        self.bind('<KeyRelease>', self.handle_keyrelease)
+        self.bind('<KeyPress>', self.handle_keypress)
+        self.bind('<Button-1>', self.show_all_options)
+
+    def set_completion_list(self, completion_list):
+        self._completion_list = sorted(completion_list)
+        self['values'] = self._completion_list
+
+    def autocomplete(self):
+        search_term = self.get().lower()
+        _hits = [item for item in self._completion_list if search_term in item.lower()]
+
+        if _hits != self._hits:
+            self._hit_index = 0
+            self._hits = _hits
+
+        if _hits:
+            self['values'] = _hits
+            self.event_generate('<Down>')
+            self.selection_clear()
+            self.select_range(self.position, tk.END)
+
+    def handle_keyrelease(self, event):
+        if event.keysym in ('BackSpace', 'Left', 'Right', 'Up', 'Down'):
+            return
+        self.autocomplete()
+
+    def handle_keypress(self, event):
+        self.position = len(self.get())
+
+    def show_all_options(self, event=None):
+        self['values'] = self._completion_list
+        self.event_generate('<Down>')
+
 class StyledDropdown:
     def __init__(self, root, options):
         self.root = root
         self.options = options
         self.selected_option = tk.StringVar()
-        
-        style = ttk.Style()
-        style.configure("TCombobox", padding=5)
-        style.configure("TLabel", font=("Helvetica", 12))
-        
+
         self.frame = ttk.Frame(root, padding="10")
         self.frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-        self.label = ttk.Label(self.frame, text="Scan:", style="TLabel")
+        self.label = ttk.Label(self.frame, text="Scan:", font=("Helvetica", 12))
         self.label.grid(row=0, column=0, padx=(0, 10), sticky=tk.W)
 
-        self.combobox = ttk.Combobox(self.frame, textvariable=self.selected_option, values=self.options, style="TCombobox")
+        self.combobox = SearchableCombobox(self.frame, textvariable=self.selected_option)
+        self.combobox.set_completion_list(self.options)
         self.combobox.grid(row=0, column=1, sticky=(tk.W, tk.E))
-        
+
         self.show_selected_button = ttk.Button(self.frame, text="Show Selected", command=self.show_selected)
         self.show_selected_button.grid(row=1, column=0, columnspan=2, pady=(10, 0))
 
@@ -27,17 +65,19 @@ class StyledDropdown:
         print("Selected item:", self.selected_option.get())
 
 root = tk.Tk()
-root.title("Styled Dropdown Menu")
+root.title("Searchable Dropdown Menu")
 root.geometry( "400x400" ) # Set the size of the window
 
 # List of 20 different options
 options = [
-    "windows.info", "windows.pslist", "windows.psscan", "windows.pstree", "windows.dumpfiles", 
-    "windows.memmap", "windows.handles", "windows.dlllist", "windows.cmdline", "windows.netscan", 
-    "windows.netstat", "windows.registry.printkey", "windows.filescan", "windows.dumpfiles"
+    "windows.pslist", "windows.psscan", "windows.pstree", "windows.cmdline",
+    "windows.services", "windows.registry", "windows.filescan", "windows.malware",
+    "windows.network", "windows.memory", "windows.disk", "windows.eventlog",
+    "windows.prefetch", "windows.timeline", "windows.locks", "windows.hooks",
+    "windows.sysinfo", "windows.drivers", "windows.pipes", "windows.sockets"
 ]
-
 sorted_list = sorted(options)
-dropdown = StyledDropdown(root, options)
+dropdown = StyledDropdown(root, sorted_list)
 
 root.mainloop()
+
