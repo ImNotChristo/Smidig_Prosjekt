b2a6d4593d823be21f0725776ef4f789e0450778
annabelleraaberg <129503939+annabelleraaberg@users.noreply.github.com>
2024-05-28 14:12:09 +0200

    Created RUN button

--- smidig projekt/runButton.py
+++ smidig projekt/runButton.py
@@ -1,10 +1,23 @@
+from tkinter import *
+
+
 class RunButton:
-    def __init__(self):
-        number = 10.0
+    def __init__(self, master):
+        self.master = master
+        self.btn = None
+        self.create_button()
 
-hello = "Hello, World!".isascii()
-print(hello)
+    def create_button(self):
+        self.btn = Button(self.master, text='RUN', command=self.master.destroy)
+        self.btn.place(x=300, y=40)
 
+
+root = Tk()
+# root.geometry('500x300')
+
+# instance of RunButton
+run_button = RunButton(root)
+root.mainloop()
 
 
 
