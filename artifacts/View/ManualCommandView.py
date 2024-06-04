b683fdd83abecf0c1d325a9fed1fa715eba18c3a
Mariataw95 <127189465+Mariataw95@users.noreply.github.com>
2024-06-04 12:51:57 +0200

    updated manual command view

--- artifacts/View/ManualCommandView.py
+++ artifacts/View/ManualCommandView.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter as ttk
+
 from Controller.ManualCommandController import ManualCommandField
 
 class ManualCommandFieldStyle:
@@ -10,5 +12,7 @@ class ManualCommandFieldStyle:
         self.entry = ManualCommandField(self.master)
         self.entry.grid(row=0, column=0, pady=10, sticky="w")
 
+        self.label = ttk.Label(self.master, text="Manual:", font=("Helvetica", 12))
+        self.label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
   
   
